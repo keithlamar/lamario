833eceee9dfd5f62a4d93836b513b43bd1e11b88
keithlamar <klgdesign@gmail.com>
2013-12-28 11:07:23 -0500

    jquery changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 	$('.circle').on('click', function(){
 		$('#projects').removeClass('small-1 small-offset-2 columns', 500);
+		$('#services').removeClass('small-4 columns', 500);
+		$('#blog').removeClass('small-1 small-offset-3 columns', 500);
+		$('#contact').removeClass('small-5 columns', 500);
+		$('#header').removeClass('small-centered', 500);
+		$('#menu').addClass('left');
+		$('#content').load('projects.html')
 	});
 
 
